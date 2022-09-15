6180c1ed93634ecd325b287fba7bfa7818d616e9
Lonny Gomes <code@lonnygomes.com>
2022-09-15 00:52:16 -0400

    feat: add random close val and refactor codebase

--- hangar_hatch/hangar_hatch/hangar_hatch.ino
+++ hangar_hatch/hangar_hatch/hangar_hatch.ino
@@ -1,21 +1,31 @@
 #include <Servo.h>
 
-#define PIN_SERVO 9
+#define PIN_SERVO 5
+#define ANGLE_OPEN 180
+#define ANGLE_CLOSE 90
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
 int pos = 0;    // variable to store the servo position
+int openDelay = 3000;
+int closeDelay;
 
+int getTimeDelay() {
+  return random(2000, 7000);
+}
 
 void setup() {
+  openDelay = getTimeDelay();
   myservo.attach(PIN_SERVO);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  myservo.write(90);
-  delay(2000);
-  myservo.write(0);
-  delay(2000);
+  myservo.write(ANGLE_OPEN); // open hatch
+  delay(openDelay);
+  
+  myservo.write(ANGLE_CLOSE); // close hatch
+  delay(closeDelay);
+  closeDelay = getTimeDelay();
 }
